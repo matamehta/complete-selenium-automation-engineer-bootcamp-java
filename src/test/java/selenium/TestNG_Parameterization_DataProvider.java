8239be7a6af41d74bfab6ad4b03723b761cb192c
Ashok Tulachan <selenium.guru.tech@gmail.com>
2018-01-23 00:41:23 -0500

    testng stuff

---
+++ src/test/java/selenium/TestNG_Parameterization_DataProvider.java
@@ -0,0 +1,66 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+import static junit.framework.TestCase.fail;
+
+
+/**
+ * Created by SeleniumGuru.com on 1/6/18.
+ */
+
+
+public class TestNG_Parameterization_DataProvider {
+    WebDriver driver = null;
+    @BeforeMethod(groups = {"Smoke Test", "Integration Test"})
+    public void beforeMethod(){
+        System.setProperty("webdriver.gecko.driver", "/Users/tulachanashok/Documents/Udemy-Selenium-Java/udemyseleniumjava/geckodriver");
+        //Initialize the selenium webdriver class and create object
+        driver = new FirefoxDriver();
+        //Go to newtours website
+        driver.get("https://newtours.herokuapp.com/");
+        //maximize the window
+        driver.manage().window().maximize();
+    }
+
+    @DataProvider(name="UserCredentials")
+    public static Object[][] getCredentials(){
+        return new Object[][] {
+                { "seleniumguru", "test1234" },
+                { "testing", "testing" },
+                { "selenium", "selenium" }
+        };
+    }
+
+    @Test(dataProvider="UserCredentials")
+    public void userLogin(String username, String password) {
+        driver.findElement(By.name("userName")).sendKeys(username);
+        //type testing in password text box
+        driver.findElement(By.name("password")).sendKeys(password);
+        //click on sign in button
+        driver.findElement(By.name("login")).click();
+        //Set Explicit Wait for 10 seconds by creating object for WebDriverWait class
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        //Now wait until next element is present
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("findFlights")));
+        //Print out the title
+        System.out.println(driver.getTitle());
+        //Validate the title
+        if (driver.getTitle().equals("Find a Flight: Mercury Tours:")) {
+            System.out.println("Test case passed.");
+        } else {
+            System.out.println("Test case failed.");
+            fail("Test case failed.");
+        }
+    }
+
+    @AfterMethod(groups = {"Smoke Test", "Integration Test"})
+    public void closeBrowser() {
+        //Destroy the driver instance and close the browser
+        driver.quit();
+    }
+}
