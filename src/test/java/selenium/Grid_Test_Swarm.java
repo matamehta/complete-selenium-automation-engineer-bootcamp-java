bb7133dd3f85067cad31da154f6855be65b35266
Ashok Tulachan <selenium.guru.tech@gmail.com>
2018-01-31 22:54:21 -0500

    Updated swarm yml

---
+++ src/test/java/selenium/Grid_Test_Swarm.java
@@ -0,0 +1,71 @@
+package selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static junit.framework.TestCase.fail;
+
+/**
+ * Created by SeleniumGuru.com on 1/6/18.
+ */
+
+
+public class Grid_Test_Swarm {
+    @Parameters({"browser","port"})
+    @Test
+    public void login_test(String browser, String port) throws InterruptedException, MalformedURLException {
+        //Initialize the hubURL with your ip address and port
+        //String hubURL = "http://192.168.1.163:".concat(port).concat("/wd/hub");
+        String hubURL = "http://192.168.99.108:4444/".concat("/wd/hub");
+        //Create firefox browser object from Desired Capabilities object
+        DesiredCapabilities capability = new DesiredCapabilities();
+        //Set Browser Name
+        capability.setBrowserName(browser);
+        //Accept Insecure Certificate
+        capability.acceptInsecureCerts();
+        //Set Platform such as Mac, Windows based on the OS you are running your test
+        capability.setPlatform(Platform.ANY);
+        //Create Remote Webdriver object using the hubUrl and capabilities object
+        WebDriver driver = new RemoteWebDriver(new URL(hubURL), capability);
+        //Go to newtours website
+        driver.get("https://newtours.herokuapp.com/");
+        //type testing in username text box
+        driver.findElement(By.name("userName")).sendKeys("testing");
+        //type testing in password text box
+        driver.findElement(By.name("password")).sendKeys("testing");
+        //click on sign in button
+        driver.findElement(By.name("login")).click();
+        //Set Explicit Wait for 10 seconds by creating object for WebDriverWait class
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        //Now wait until next element is present
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("findFlights")));
+        //Print out the title
+        System.out.println(driver.getTitle());
+
+        //Validate the title
+        if (driver.getTitle().equals("Find a Flight: Mercury Tours:")){
+            System.out.println("Test case passed.");
+        } else {
+            System.out.println("Test case failed.");
+            fail("Test case failed.");
+        }
+        //Destroy the driver instance and close the browser
+        driver.quit();
+    }
+}
+
+
+
+
+
